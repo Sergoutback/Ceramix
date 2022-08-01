0f20a78761044e4753fc09b3b1e65234d7f3b32f
sergoutback <webart2013@gmail.com>
2022-08-01 20:58:33 +0300

    01072022_2058
    Try to fill Square

--- Assets/Scripts/TailCalculator.cs
+++ Assets/Scripts/TailCalculator.cs
@@ -5,7 +5,11 @@ using UnityEngine.UI;
 
 public class TailCalculator : MonoBehaviour
 {
-    public GameObject Tail;
+    public GameObject tail;
+
+    public Vector3 tailPosition;
+
+    private GameObject _newTail;
 
     //[SerializeField] private GameObject BigSquare;
     //[SerializeField] private int width;
@@ -17,7 +21,7 @@ public class TailCalculator : MonoBehaviour
     {
         FillSquare();
 
-        TailPosition();
+        //TailPosition();
     }
 
 
@@ -31,7 +35,9 @@ public class TailCalculator : MonoBehaviour
             for (int w = 0; w < 5; ++w)
             {
                 //Tail.AddComponent<TailCalculator>();
-                TailCalculator tail = Tail.GetComponent<TailCalculator>();
+
+                //TailCalculator tail = this.prefabTail.GetComponent<TailCalculator>();
+
                 //tail.transform.position = new Vector3(250, 40, 100);
                 //Tail.AddComponent<Transform>().Init();
                 //var tail = new GameObject();
@@ -41,7 +47,27 @@ public class TailCalculator : MonoBehaviour
 
                 //tail.transform.position = Vector3.MoveTowards(Tail.transform.position, transform.position + Vector3.up, 1f);
 
-                Debug.Log("newTail.transform.position= " + tail.transform.position);
+                //Debug.Log("newTail.transform.position= " + this.Tail.transform.position);
+
+                //GameObject TailInstance = GameObject.Instantiate(tail) as GameObject;
+
+                TailClone();
+
+                //TailPosition();
+
+
+
+
+
+                //Transform TailInstanceTransformPosition = TailInstance.GetComponent<Transform>();
+
+                //TailInstance.transform.position = new Vector3(TailInstanceTransformPosition.position.x + 100f, TailInstanceTransformPosition.position.y, TailInstanceTransformPosition.position.z);
+
+                //Debug.Log("prefabInstance.transform.position= " + TailInstance.transform.position);
+
+                //GameObject instanceThroughReference = GameObject.Instantiate(aReferenceToAnInstantiableObject) as GameObject;
+
+                //GameObject Tail = new GameObject("Tail");
             }
         }
     }
@@ -49,10 +75,27 @@ public class TailCalculator : MonoBehaviour
     //{
     //    Tail = Instantiate(Tail, new Vector3(-240, -440, 0), Quaternion.identity);
 
-        
+
     //}
-    private void TailPosition()
+    private void TailClone()
     {
-        Transform tailPosition = Tail.GetComponent<Transform>();
+
+        Vector3 tailPos = new Vector3(tailPosition.x, tailPosition.y, tailPosition.z);
+        
+        var _newTail = Instantiate(tail, tailPos, Quaternion.identity);
+
+        //newTail.transform.localPosition = new Vector3(tailPosition.x + 10f, tailPosition.y, tailPosition.z);
+
+        _newTail.transform.localPosition = new Vector3(tailPos.x + 10f, tailPos.y, tailPos.z);
+        Debug.Log("newTail = " + _newTail.transform.position);
+
+
+        //Transform tailPosition = prefabTail.GetComponent<Transform>();
     }
+
+    //private void TailPosition()
+    //{
+    //    this._newTail.transform.localPosition = new Vector3(tailPos.x + 10f, tailPos.y, tailPos.z);
+    //    Debug.Log("newTail = " + _newTail.transform.position);
+    //}
 }
