66454c8534f06bf88d9a4b0710666ffbfb464c39
sergoutback <webart2013@gmail.com>
2022-08-02 18:55:19 +0300

    02072022_1854
    Control save

--- Assets/Scripts/TailCalculator.cs
+++ Assets/Scripts/TailCalculator.cs
@@ -5,97 +5,116 @@ using UnityEngine.UI;
 
 public class TailCalculator : MonoBehaviour
 {
-    public GameObject tail;
+    [SerializeField] private GameObject parent;
 
-    public Vector3 tailPosition;
+    [SerializeField] private GameObject tail;
 
-    private GameObject _newTail;
+    //private Vector3 tailPosition;
 
-    //[SerializeField] private GameObject BigSquare;
-    //[SerializeField] private int width;
-    //[SerializeField] private int height; 
 
-    //private int[,] fillBigSquare;
 
     void Start()
     {
         FillSquare();
-
-        //TailPosition();
     }
 
 
     private void FillSquare()
     {
-        //fillBigSquare = new int [width, height];
-        //Tail.SetActive(true);
-
         for (int h = 0; h < 5; ++h)
         {
             for (int w = 0; w < 5; ++w)
             {
-                //Tail.AddComponent<TailCalculator>();
+                Vector3 tailPosition = new Vector3();
 
-                //TailCalculator tail = this.prefabTail.GetComponent<TailCalculator>();
+                tailPosition = new Vector3(tailPosition.x, tailPosition.y, 0f);
 
-                //tail.transform.position = new Vector3(250, 40, 100);
-                //Tail.AddComponent<Transform>().Init();
-                //var tail = new GameObject();
+                if (tailPosition.y > -450f || tailPosition.y < 50f)
 
-                //Tail.transform.position = Vector3.MoveTowards(Tail.transform.position, Vector3(250, 40, 100), 1f);
-                //InstantiateTail();
-
-                //tail.transform.position = Vector3.MoveTowards(Tail.transform.position, transform.position + Vector3.up, 1f);
-
-                //Debug.Log("newTail.transform.position= " + this.Tail.transform.position);
-
-                //GameObject TailInstance = GameObject.Instantiate(tail) as GameObject;
-
-                TailClone();
-
-                //TailPosition();
+                    tailPosition.y += 100f;
+                else
+                    tailPosition.y = -450f;
 
 
+                if (tailPosition.x > -250f || tailPosition.x < 250f)
 
+                    tailPosition.x += 100f;
+                else
+                    tailPosition.x = -250f;
 
+                GameObject newTail = Instantiate(tail, tailPosition, Quaternion.identity) as GameObject;
 
-                //Transform TailInstanceTransformPosition = TailInstance.GetComponent<Transform>();
+                newTail.transform.SetParent(parent.transform, false);
 
-                //TailInstance.transform.position = new Vector3(TailInstanceTransformPosition.position.x + 100f, TailInstanceTransformPosition.position.y, TailInstanceTransformPosition.position.z);
+                newTail.transform.localScale = new Vector3(1, 1, 1);
 
-                //Debug.Log("prefabInstance.transform.position= " + TailInstance.transform.position);
-
-                //GameObject instanceThroughReference = GameObject.Instantiate(aReferenceToAnInstantiableObject) as GameObject;
-
-                //GameObject Tail = new GameObject("Tail");
+                newTail.transform.position = tailPosition;
             }
         }
     }
+
+    //private void TailClone()
+    //{
+
+    //    tailPosition = new Vector3(this.tail.transform.position.x, this.tail.transform.position.y, this.tail.transform.position.z);
+
+    //    GameObject tail = Instantiate(this.tail, tailPosition, Quaternion.identity) as GameObject;
+
+    //    tail.transform.parent = transform;
+
+
+        //_newTail = Instantiate(tail, tailPosition, Quaternion.identity);
+
+        //newTail.transform.localPosition = new Vector3(tailPosition.x + 10f, tailPosition.y, tailPosition.z);
+
+        //tail.transform.localPosition = new Vector3(tailPosition.x, tailPosition.y, tailPosition.z);
+
+        //Debug.Log("newTail = " + tail.transform.localPosition);
+
+
+        //Transform tailPosition = prefabTail.GetComponent<Transform>();
+
     //private void InstantiateTail()
     //{
     //    Tail = Instantiate(Tail, new Vector3(-240, -440, 0), Quaternion.identity);
 
 
     //}
-    private void TailClone()
-    {
-
-        Vector3 tailPos = new Vector3(tailPosition.x, tailPosition.y, tailPosition.z);
-        
-        var _newTail = Instantiate(tail, tailPos, Quaternion.identity);
-
-        //newTail.transform.localPosition = new Vector3(tailPosition.x + 10f, tailPosition.y, tailPosition.z);
-
-        _newTail.transform.localPosition = new Vector3(tailPos.x + 10f, tailPos.y, tailPos.z);
-        Debug.Log("newTail = " + _newTail.transform.position);
-
-
-        //Transform tailPosition = prefabTail.GetComponent<Transform>();
-    }
 
     //private void TailPosition()
     //{
     //    this._newTail.transform.localPosition = new Vector3(tailPos.x + 10f, tailPos.y, tailPos.z);
     //    Debug.Log("newTail = " + _newTail.transform.position);
     //}
+
+    //Tail.AddComponent<TailCalculator>();
+
+    //TailCalculator tail = this.prefabTail.GetComponent<TailCalculator>();
+
+    //tail.transform.position = new Vector3(250, 40, 100);
+    //Tail.AddComponent<Transform>().Init();
+    //var tail = new GameObject();
+
+    //Tail.transform.position = Vector3.MoveTowards(Tail.transform.position, Vector3(250, 40, 100), 1f);
+    //InstantiateTail();
+
+    //tail.transform.position = Vector3.MoveTowards(Tail.transform.position, transform.position + Vector3.up, 1f);
+
+    //Debug.Log("newTail.transform.position= " + this.Tail.transform.position);
+
+    //GameObject TailInstance = GameObject.Instantiate(tail) as GameObject;
+
+    //TailPosition();
+
+    //Transform TailInstanceTransformPosition = TailInstance.GetComponent<Transform>();
+
+    //TailInstance.transform.position = new Vector3(TailInstanceTransformPosition.position.x + 100f, TailInstanceTransformPosition.position.y, TailInstanceTransformPosition.position.z);
+
+    //Debug.Log("prefabInstance.transform.position= " + TailInstance.transform.position);
+
+    //GameObject instanceThroughReference = GameObject.Instantiate(aReferenceToAnInstantiableObject) as GameObject;
+
+    //GameObject Tail = new GameObject("Tail");
+    //}
+
 }
